128d2856d88d0e3cf655b71e240a8a564a21a814
Brenda Moreira Santos <brendamoreira@Brendas-MacBook-Air.local>
2023-11-17 15:55:14 +0100

    site funcionando sem css arrumado

--- pilote/views.py
+++ pilote/views.py
@@ -19,14 +19,19 @@ def change_ok(etat, nouveau_lieu):
 
     return ok
 
-def changement_etat(lieu):
-    if lieu == 'pit':
-        etat = "gymnasium"
+def changer_etat(lieu):
+    if lieu == 'vestiaire':
+        etat = "pas préparé"
 
     elif lieu == 'gymnasium':
-        etat = "Fatigué"
+        etat = "prêt"
 
-        
+    elif lieu == 'pit':
+        etat = "énergétique"
+
+    elif lieu == 'piste de course':
+        etat = "Fatigué"
+  
     return etat
 
 def character_detail(request, id_character):
@@ -50,16 +55,18 @@ def character_detail(request, id_character):
                     occupants = []
                     pilotes = Character.objects.all().order_by('id_character')
                     for pilote in pilotes:
-                        if character_x.lieu.id_equip == "piste de course":
+                        if pilote.lieu.id_equip == "piste de course":
                             occupants.append(pilote.id_character)
+
                     if len(occupants) == 2:
                         nouveau_lieu.disponibilite = "occupé"
+
                     else:
-                            nouveau_lieu.disponibilite = "libre"
+                        nouveau_lieu.disponibilite = "libre"
                 
                 nouveau_lieu.save()
                 character.lieu = nouveau_lieu
-                #character.etat = change_ok(character.lieu.id_equip)
+                character.etat = changer_etat(character.lieu.id_equip)
                 character.save()
                 message = f"{character.id_character} est dans le lieu démandé ({nouveau_lieu.id_equip})"
                 form.save()
@@ -72,10 +79,10 @@ def character_detail(request, id_character):
                 for pilote in pilotes:
                     if pilote.lieu.id_equip == nouveau_lieu.id_equip:
                         occupants.append(pilote.id_character)
-                message = f"{nouveau_lieu.id_equip} est deja occupé(e) par {','.join(occupants)}"
+                message = f"La piste de course est deja occupé par {' et '.join(occupants)}!"
         else:
             error = True
-            message = f"{character.id_character} ne peut pas aller au lieu demandé ({nouveau_lieu.id_equip}) dans son état actuel ({character.etat})"
+            message = f"{character.id_character} ne peut pas aller au lieu demandé {nouveau_lieu.id_equip}! Il est {character.etat} pour ça :("
         return render(request,
             'pilote/character_detail.html',
             {'character': character, 'message': message, 'error': error, 'lieu': ancien_lieu, 'nouveau_lieu': nouveau_lieu, 'form': form})
